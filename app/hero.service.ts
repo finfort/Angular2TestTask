9ad469467644144ff67915f6da1aa8fd3adcb3e1
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2016-03-16 13:45:04 +0300

    add marks to map

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,25 +6,25 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class HeroService {
-  constructor (private http: Http) {}
+    constructor(private http: Http) { }
 
-  private _token = "token=YIu7Ofztb9AzTZ0qi7HW58ZsBTZH9tV4";  
-  private _heroesUrl = 'http://test-api.live.gbksoft.net/api/v1/users?'+this._token;  
-  private _heroeUrl = 'http://test-api.live.gbksoft.net/api/v1/users/';
-  // URL to web api
-  getHeroes () {
-    return this.http.get(this._heroesUrl)
-                    .map(res => <Hero[]> res.json().result)
-                    .catch(this.handleError);
-  }
- private handleError (error: Response) {
-    console.error(error);
-    return Observable.throw(error.json().error || 'Server error');
-  }
-  getHero(id: number) {
-      //urgently should change this
-     return this.http.get(this._heroeUrl + id +"?" + this._token)
-                    .map(res => <Hero[]> res.json().result)
-                    .catch(this.handleError);
-  }
+    private _token = "token=YIu7Ofztb9AzTZ0qi7HW58ZsBTZH9tV4";
+    private _heroesUrl = 'http://test-api.live.gbksoft.net/api/v1/users?' + this._token;
+    private _heroeUrl = 'http://test-api.live.gbksoft.net/api/v1/users/';
+    // URL to web api
+    getHeroes() {
+        return this.http.get(this._heroesUrl)
+            .map(res => <Hero[]>res.json().result)
+            .catch(this.handleError);
+    }
+    private handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+    getHero(id: number) {
+        //urgently should change this
+        return this.http.get(this._heroeUrl + id + "?" + this._token)
+            .map(res => <Hero[]>res.json().result)
+            .catch(this.handleError);
+    }
 }
